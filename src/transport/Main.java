a512469f26d5f5a590fcf853970642a140a64f7d
azamat85 <8564896@mail.ru>
2022-08-14 23:27:34 +0300

    commint123

--- src/transport/Main.java
+++ src/transport/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
        Car car1 = new Car("bmv","x5","black",240.8);
        Car car2 = new Car("audi","Q7","black",253.4);
+       Car car5 = new Car("audi","Q7","black",253.4);
        car1.abs();
        Cabriolet car3 = new Cabriolet();
        car3.roofRemoved();
